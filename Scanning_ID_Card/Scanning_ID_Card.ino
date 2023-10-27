7c6cefc8e465fc82237385dfdaea54987c0e48da
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2023-10-27 16:33:42 +0530

    Updated the scanning ino file

--- Scanning_ID_Card/Scanning_ID_Card.ino
+++ Scanning_ID_Card/Scanning_ID_Card.ino
@@ -2,62 +2,62 @@
 #include <WiFiUdp.h>
 #include <MFRC522.h>
 #include <SPI.h>
- 
-#define RST_PIN         D3           
-#define SS_PIN          D4  
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   
+#define RST_PIN         D3
+#define SS_PIN          D4
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
 
-const char* ssid = "OPPO F19";
-const char* password = "swati191203";
-const char* host = "10.1.44.31";  
-unsigned int localPort = 8888;  
+const char* ssid = "tae";
+const char* password = "khyathi1234";
+const char* host = "192.168.95.218";  // Change to the IP address of your Python server
+unsigned int localPort = 8888;  // Change the port number
 
 WiFiUDP udp;
 
 void setup() {
   Serial.begin(9600);
-  SPI.begin(); 
+  SPI.begin();
   udp.begin(localPort);
-  mfrc522.PCD_Init(); 
+  mfrc522.PCD_Init();
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
-  Serial.println(F("Read personal data on a MIFARE PICC:")); 
+  Serial.println(F("Read personal data on a MIFARE PICC:"));
 }
 
 void loop() {
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
- 
+
   byte block;
   byte len;
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
 
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
- 
+
   Serial.println(F("Card Detected:"));
- 
+
   byte buffer1[18];
- 
+
   block = 1;
   len = 18;
- 
+
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Authentication failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
- 
+
   status = mfrc522.MIFARE_Read(block, buffer1, &len);
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Reading failed: "));
@@ -65,16 +65,14 @@ void loop() {
     return;
   }
   char studentID[14];
-  for (uint8_t i = 0; i < 13; i++)
-  {
-      studentID[i] = (char)buffer1[i];
+  for (uint8_t i = 0; i < 13; i++) {
+    studentID[i] = (char)buffer1[i];
   }
-  for (uint8_t i = 0; i < 13; i++)
-  {
-      Serial.print(studentID[i]);
+  for (uint8_t i = 0; i < 13; i++) {
+    Serial.print(studentID[i]);
   }
   Serial.println();
-  studentID[14]='\0';
+  studentID[14] = '\0';
   udp.beginPacket(host, localPort);
   udp.write(studentID);
   udp.endPacket();
